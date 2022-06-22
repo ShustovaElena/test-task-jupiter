d3a175f4ad286f17aaae0239e8e2d25f9c0f3944
Elena Shustova <alenyf-ka@mail.ru>
2022-06-22 11:37:36 +0300

    feat: divide header to components

--- test-task/src/components/Header/index.tsx
+++ test-task/src/components/Header/index.tsx
@@ -1,24 +1,12 @@
+import Main from "../Main";
+import Menu from "../Menu";
 import "./styles.css";
 
 const Header = () => {
   return (
     <div className="Header">
-      <img className="Logo" src="./logo.png" alt="logo" />
-      <nav className="Navbar">
-        <a className="Link" href="#">
-          About
-        </a>
-        <a className="Link" href="#">
-          Services
-        </a>
-        <a className="Link" href="#">
-          Pricing
-        </a>
-        <a className="Link" href="#">
-          Blog
-        </a>
-      </nav>
-      <button className="Contact-button">Contact</button>
+      <Menu />
+      <Main />
     </div>
   );
 };
