52126a6e3ad3edea784e9517a4ef9e589ead8469
Elena Shustova <alenyf-ka@mail.ru>
2022-06-23 15:10:02 +0300

    feat: add ability change border to active card

--- test-task/src/components/Card/index.tsx
+++ test-task/src/components/Card/index.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { ICard } from "../../types";
 import "./styles.css";
 
 const Card = (props: ICard) => {
   const { src, name, category } = props;
+  const [isActive, setIsActive] = useState(false);
+
+  const addActiveClass = () => {
+    setIsActive(!isActive);
+  };
 
   return (
-    <div className="Card" style={{ backgroundImage: `url("./img/${src}")` }}>
+    <div
+      className="Card"
+      style={{
+        backgroundImage: `url("./img/${src}")`,
+        border: isActive ? "4px solid #21cf5a" : "none",
+      }}
+      onClick={addActiveClass}
+    >
       <button className="Category-button">{category}</button>
       <p className="Card-name">{name}</p>
     </div>
